949f3064ffc60adbde7ef68d73250ace5fc20eec
garrettbadger <garrett.badger.id@gmail.com>
2023-05-20 12:25:27 -0600

    week 5 update

--- RMR/screens/MealScreen.js
+++ RMR/screens/MealScreen.js
@@ -1,16 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard } from 'react-native';
+import React, { useState } from 'react';
+import Recipe from '../components/Recipe';
 
 
 export default function MealScreen() {
+  const [recipe, setRecipe] = useState('');
+  const [recipeBox, setRecipeBox] = useState([]);
+
+  const handleRecipeInput = (input) => {
+        
+    setRecipe(input);
+    
+  };
+  const inputRecipe= () => {
+    if (recipe != '') {
+      Keyboard.dismiss()
+      setRecipeBox([...recipeBox, recipe]);
+      setRecipe('');
+    }
+  }
+
+  const completeRecipe = (index) => {
+    let itemsCopy = [...recipeBox];
+    itemsCopy.splice(index, 1);
+    setRecipeBox(itemsCopy);
+  }
   return (
     <View style={styles.container}>
       <View style = {styles.tasksWrapper}>
-        <Text style = {styles.sectionTitle}>Today's tasks</Text>
+        <Text style = {styles.sectionTitle}>Recipes</Text>
         <View style = {styles.items}>
-          <Text>"Hello I Am MealScreen"</Text>
+          {
+            recipeBox.map((item, index) =>{
+              return (
+                <TouchableOpacity onPress={() => completeRecipe(index)}>
+                  <Recipe key={index} text={item}/>
+                </TouchableOpacity>
+                
+              )
+            })
+          }
+          
         </View>
       </View>
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.inputRecipe}
+      >
+        
+        <TextInput
+        style={styles.input} 
+        placeholder={'Input Recipe'}
+        keyboardType="default"
+        value={recipe}
+        onChangeText={recipe => handleRecipeInput(recipe)}
+        />
+        <TouchableOpacity onPress={() => inputRecipe()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>ADD</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+      
 
     </View>
   );
@@ -23,14 +75,59 @@ const styles = StyleSheet.create({
     
   },
   tasksWrapper: {
-    paddingTop: 80,
+    paddingTop: 0,
     paddingHorizontal: 20,
+    width: '100%', 
+    height: 600, 
+    backgroundColor: '#fff', 
+    borderRadius: 80, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    borderColor: '#C0C0C0', 
+    borderWidth: 1,
+    position: 'absolute',
+    // bottom: 500,
+    right: 0
   },
   sectionTitle:{
     fontSize: 24,
     fontWeight: 'bold',
+    position: 'absolute',
+    paddingBottom: 100, 
+    
   },
   items:{
     marginTop:30,
   },
+  inputRecipe:{
+    position: 'absolute',
+    bottom: 0, 
+    width: '100%', 
+    flexDirection: 'row', 
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15 ,
+    backgroundColor: '#fff', 
+    borderRadius: 60, 
+    borderColor: '#C0C0C0', 
+    borderWidth: 1,
+    width: 250,
+
+  },
+  addWrapper:{
+    width: 60, 
+    height: 60, 
+    backgroundColor: '#fff', 
+    borderRadius: 60, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    borderColor: '#C0C0C0', 
+    borderWidth: 1,
+    
+  },
+  
 });
--- RMR/screens/StudyScreen.js
+++ RMR/screens/StudyScreen.js
@@ -1,17 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+
+
+
+
 
 
 export default function StudyScreen() {
+   
+    const [number, setNumber] = useState(0);
+    const [goal, setGoal] = useState(0);
+    const [studied, setStudied] = useState(0);
+    const [timeStudied, setTimeStudied] = useState(0);
+    
+    const handleNumberInput = (input) => {
+      // Remove non-numeric characters from the input
+      const cleanedInput = input.replace(/[^0-9]/g, '');
+      setNumber(cleanedInput);
+      
+    };
+    const handleStudiedInput = (input) => {
+      // Remove non-numeric characters from the input
+      const cleanedInput = input.replace(/[^0-9]/g, '');
+      setStudied(cleanedInput);
+      
+    };
+    
+    const logInput = () => {
+      if (number != 0) {
+        setGoal(number)
+        setNumber(0);
+      }
+      
+    };
+    const inputStudied = () => {
+      if (studied != 0) {
+        setTimeStudied(studied);
+        setStudied(0);
+      }
+      
+    }
   return (
     <View style={styles.container}>
       <View style = {styles.tasksWrapper}>
         <Text style = {styles.sectionTitle}>Today's tasks</Text>
         <View style = {styles.items}>
-          <Text>"Hello I Am StudyScreen"</Text>
+          <Text>"Goal: {goal}m Read: {timeStudied}m Time left: {goal - timeStudied}m"</Text>
         </View>
       </View>
 
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.inputStudyTime}
+      >
+        
+        <TextInput
+        style={styles.input} 
+        placeholder={'Input study time goal'}
+        keyboardType="numeric"
+        value={number}
+        onChangeText={number => handleNumberInput(number)}
+        />
+        <TouchableOpacity onPress={() => logInput()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>ADD</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.inputStudiedTime}
+      >
+        
+        <TextInput
+        style={styles.input} 
+        placeholder={'Input time studied'}
+        keyboardType="numeric"
+        value={studied}
+        onChangeText={studied => handleStudiedInput(studied)}
+        />
+        <TouchableOpacity onPress={() => inputStudied()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>ADD</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -23,8 +97,20 @@ const styles = StyleSheet.create({
     
   },
   tasksWrapper: {
-    paddingTop: 80,
+    paddingTop: 0,
     paddingHorizontal: 20,
+    width: 250, 
+    height: 200, 
+    backgroundColor: '#fff', 
+    borderRadius: 80, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    borderColor: '#C0C0C0', 
+    borderWidth: 1,
+    position: 'absolute',
+    bottom: 450,
+    right: 80
+    
   },
   sectionTitle:{
     fontSize: 24,
@@ -33,4 +119,44 @@ const styles = StyleSheet.create({
   items:{
     marginTop:30,
   },
+  inputStudyTime:{
+    position: 'absolute',
+    bottom: 60, 
+    width: '100%', 
+    flexDirection: 'row', 
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20,
+  },
+  inputStudiedTime:{
+    position: 'absolute',
+    bottom: 150, 
+    width: '100%', 
+    flexDirection: 'row', 
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15 ,
+    backgroundColor: '#fff', 
+    borderRadius: 60, 
+    borderColor: '#C0C0C0', 
+    borderWidth: 1,
+    width: 250,
+
+  },
+  addWrapper:{
+    width: 60, 
+    height: 60, 
+    backgroundColor: '#fff', 
+    borderRadius: 60, 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    borderColor: '#C0C0C0', 
+    borderWidth: 1,
+    
+  },
+  addText:{},
 });
